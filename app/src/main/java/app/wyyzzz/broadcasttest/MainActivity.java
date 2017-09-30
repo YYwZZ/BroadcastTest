3ce3c1250f57ae311bced468d572f6c9a660a359
YYwZZ <1195071851@qq.com>
2017-09-30 09:07:43 +0800

    second commit

--- app/src/main/java/app/wyyzzz/broadcasttest/MainActivity.java
+++ app/src/main/java/app/wyyzzz/broadcasttest/MainActivity.java
@@ -6,33 +6,66 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private IntentFilter intentFilter;
 
-    private NetworkChangeReceiver networkChangeReceiver;
+    private LocalReceiver localReceiver;
+
+    private LocalBroadcastManager localBroadcastManager;
+
+    //private NetworkChangeReceiver networkChangeReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        intentFilter = new IntentFilter();
+        localBroadcastManager = LocalBroadcastManager.getInstance(this);
+        /*intentFilter = new IntentFilter();
         intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
         networkChangeReceiver = new NetworkChangeReceiver();
-        registerReceiver(networkChangeReceiver, intentFilter);
+        registerReceiver(networkChangeReceiver, intentFilter);*/
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent("app.wyyzzz.broadcasttest.LOCAL_BROADCAST");
+                localBroadcastManager.sendBroadcast(intent);//发送本地广播
+            }
+        });
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("app.wyyzzz.broadcasttest.LOCAL_BROADCAST");
+        localReceiver = new LocalReceiver();
+        localBroadcastManager.registerReceiver(localReceiver,intentFilter);//注册本地广播监听器
     }
+
+    /*public void btn1(View view){
+        Intent intent = new Intent("app.wyyzzz.broadcasttest.MY_BROADCAST");
+        sendOrderedBroadcast(intent,null);
+
+    }*/
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        unregisterReceiver(networkChangeReceiver);
+        localBroadcastManager.unregisterReceiver(localReceiver);
     }
 
-    class NetworkChangeReceiver extends BroadcastReceiver{
+    class LocalReceiver extends BroadcastReceiver{
+        @Override
+        public void onReceive(Context context,Intent intent){
+            Toast.makeText(context,"received local broadcast",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /*class NetworkChangeReceiver extends BroadcastReceiver{
 
         @Override
         public void onReceive(Context context, Intent intent){
@@ -44,5 +77,5 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(context,"network is unavailable",Toast.LENGTH_SHORT).show();
             }
         }
-    }
+    }*/
 }
